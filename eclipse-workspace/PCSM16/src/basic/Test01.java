763b5492f3c859ccc08de039696e0dfb280544e5
Mahesh Kumbhar <113030145+maheshdk29@users.noreply.github.com>
2022-11-13 00:08:20 +0530

    Update Test01.java

--- eclipse-workspace/PCSM16/src/basic/Test01.java
+++ eclipse-workspace/PCSM16/src/basic/Test01.java
@@ -3,7 +3,7 @@ package basic;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class PageSource {
+public class Test01 {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
